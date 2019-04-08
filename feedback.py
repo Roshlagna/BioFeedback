ab32e38201c434d4b1a63415fd660a0b7a58da60
Roshlagna <rgs5174@psu.edu>
2019-04-08 10:15:40 -0400

    Add files via upload

--- feedback.py
+++ feedback.py
@@ -4,29 +4,85 @@ from matplotlib import pyplot as plt
 from matplotlib import style
 import numpy as np
 
-def graphCSV():
-	style.use('ggplot')
+def csvData():
 	with open("1.csv", 'rt') as f:
     		csv_reader = csv.reader(f)
-    
-    		for line in csv_reader:
-        		if (line[2]) == "TOCO":
-        			for data in line[3:]:
-        	        		if (data == ''):
-        	            			next 
-        	        		else:
-        	        	    		data = np.loadtxt('1.csv', unpack = True, delimiter = 'TOCO')
-        		else:
-        	    		next
+
+    		maxpressure = 0
+    		xval = 0
+		xreg = 0
+		with open("xyData.txt", 'wt') as wl:
+    			for line in csv_reader:
+			
+        			if (line[2]) == "TOCO":
+        				for data in line[3:]:
+						xval = xval + 1
+						if(data == ''):
+							next
+						elif(int(data) < 25):
+							strength = int(data)
+							l1Push(strength)
+							wl.write(data)
+							wl.write('\n')
+
+						elif(int(data) < 50):
+							strength = int(data)
+							l2Push(strength)
+							wl.write(data)
+							wl.write('\n')
+						elif(int(data) > 70):
+							print(data)
+							wl.write(data)
+							wl.write('\n')
+							print("Contraction on the rise and occurring!!")
+							if(int(data) in range(97,100)):
+								print("Peak contraction here! Showing push strength")
+								contraction = int(data)
+								wl.write(data)
+								wl.write('\n')
+								#bio_feedback(contraction) 
+        	        			else:
+        	        	    			print(data)							
+							wl.write(data)
+							wl.write('\n')
+							        	        		
+        			else:
+        	    			next
+			#print("X value is:", xval)
+			#print("data region value is:", xreg)
+			#print("The max pressure was", maxpressure)
 
 
-graphCSV()
-style.use('ggplot')
-x,y = np.loadtxt('example.txt', unpack = True, delimiter = ',')
+def l1Push(strength):
+	print("Your push strength is at a cool: ", strength)
+	
+	
+def l2Push(strength):
+	
+	print("A contraction may be on the rise")
+	print(strength)
+	
 
-plt.plot(x,y)
-plt.title('Feedback')
-plt.xlabel('Time')
-plt.ylabel('Pressure')
-plt.show()
+def l3Push(strength):
+	print("You are entering the contraction")
+
+
+def graph():
+	style.use('ggplot')
+
+	x = np.loadtxt('xyData.txt', unpack = True, delimiter = ',')
+
+	plt.plot(x)
+	plt.title('Feedback')
+	plt.xlabel('Time')
+	plt.ylabel('Pressure')
+
+
+	plt.show()
+
+
+
+
+csvData()
+graph()
 
