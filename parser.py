7cbac55f612b5142ba7b9d6d677ee26894b21693
Roshlagna <rgs5174@psu.edu>
2019-04-24 14:38:05 -0400

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,10 +1,15 @@
 import csv
 import time
 from turtle import *
+from Tkinter import *
 from matplotlib import pyplot as plt
 from matplotlib import style
 import numpy as np
 import turtle
+import random
+
+
+
 def csvData():
 	with open("1.csv", 'rt') as f:
     		csv_reader = csv.reader(f)
@@ -77,13 +82,39 @@ def timing():
 	y= y + 100
 	time.sleep(1)
 
+def demo1():
+	with open("example.txt", 'r') as ex:
+		x = -400
+		y = -400
+		i = 1
+		for data, line in enumerate(ex):
+			if(data == (4*i)):
+				print(line)
+				i = i+1
+				maxPush = 99
+				if( x >= 500):
+					x = 500						
+				
+				if(int(data) >= 99):
+					#x = 100
+					drawing(x)
+					x = x + 100
+				if(int(data) <= 80):
+					#closeWindow(bye)
+					x = y
+					bye = 0
+					print("GOT YOUR STATEMENT")
+					#time.sleep(1)
+			
+
 def demo():
 	with open("example.txt", 'r') as ex:
-		x = 0
-		y = -500
+		x = -400
+		y = -400
 		i = 1
 		num = 0
 		pp = 0
+		bye = 1
 		for data, line in enumerate(ex):
 			if(data == (4*i)):
 				i = i + 1
@@ -91,43 +122,37 @@ def demo():
 				
 				print("Data value is:", int(line))
 					
-				if(y <= 400):				
-					if(int(line) >= 99):	
-						pp = int(line)	
-						x = int(y)
-						drawing(x)
-						y= y + 100
-				#else:
-					#time.sleep(1)
-						
-				
-				#time.sleep(1)
+				if( x >= 320):
+					x = 320		
+				if(int(line) >= 99):	#maxVal	pp = peak pressure	
+					#pp = int(line)	
+					drawing(x)
+					x= x + 80
+					bye = 1
+					
+				if(int(line) <= 80):
+					closeWindow(bye)									
+					x = y
+					bye = 0
+					print("Regulated pressure")
+					
+					
 				
 		print(num)		
 				
 	graph()
 
-def demo1():
-	with open("example.txt", 'r') as ex:
-		x = 0
-		y = -600
-		i = 1
-		for data, line in enumerate(ex):
-			if(data == (4*i)):
-				print(line)
-				i = i+1
-				maxPush = 99
-				if(int(data) > 99):
-					x = 100
-					drawing(x)
-					x = x + 100
+		
+						
 				
 				
 	
-def refresh(x):
-	bgcolor("white")
-	title("BioFeedback")
-
+def closeWindow(bye):
+	if(bye == 1):
+		turtle.clear()
+	else:
+		bye = 0
+	bye = 0
 
 def drawing(x):
 	window = turtle.Screen()
@@ -138,32 +163,37 @@ def drawing(x):
 	turtle.up()
 	turtle.goto(x,-50)
 	turtle.down()
+	turtle.hideturtle()
 	turtle.pencolor("white")
 	turtle.fillcolor("green")
 	turtle.begin_fill()
 	t0 = time.time()
 	
-	turtle.forward(80)
+	turtle.forward(60)
 	turtle.left(90)
-	turtle.forward(80)
+	turtle.forward(60)
 	turtle.left(90)
-	turtle.forward(80)
+	turtle.forward(60)
 	turtle.left(90)
-	turtle.forward(80)
+	turtle.forward(60)
 	turtle.left(90)
 	turtle.end_fill()	
 	#turtle.bgcolor("white")
 	time.sleep(.1)
 	print(time.time() - t0)
-	#window.exitonclick()
+
 
 	
-	
+def spirals():
+	t = turtle.Turtle()
+	for i in range(500):
+		t.forward(i)
+		t.left(91)
+	turtle.done()
 
-#csvData()
-#testFeed()
-#timing()
-#graph()
+
 demo()
-#drawing()
+#spirals()
+
+
 
